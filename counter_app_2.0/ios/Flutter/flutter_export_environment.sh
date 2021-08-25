12178c7354260e469cb15dca38487f830dec9568
Andy <andyzhao0808@gmail.com>
2021-08-25 16:30:20 +0700

    initial commit: basic files;

---
+++ counter_app_2.0/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/panzonghua/company/sdk/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/panzonghua/Downloads/source-code-1/chapter_3/counter_app_2.0"
+export "FLUTTER_TARGET=/Users/panzonghua/Downloads/source-code-1/chapter_3/counter_app_2.0/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=flutter.inspector.structuredErrors%3Dtrue"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/panzonghua/Downloads/source-code-1/chapter_3/counter_app_2.0/.dart_tool/package_config.json"
---
+++ counter_app_2.0/lib/fancy_button.dart
@@ -0,0 +1,69 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class FancyButton extends StatefulWidget {
+  final VoidCallback onPressed;
+  final Widget child;
+
+  const FancyButton({
+    Key key,
+    this.onPressed,
+    this.child,
+  }) : super(key: key);
+
+  @override
+  _FancyButtonState createState() => _FancyButtonState();
+}
+
+class _FancyButtonState extends State<FancyButton> {
+  Color _getColors() {
+    return _buttonColors.putIfAbsent(this, () => colors[next(0, 5)]);
+  }
+
+
+  @override
+  void didUpdateWidget(covariant FancyButton oldWidget) {
+    debugPrint("oldWidget.child != widget.child=${oldWidget.child != widget.child}");
+    if(oldWidget.child != widget.child) {
+      setState(() {
+
+      });
+    }
+    super.didUpdateWidget(oldWidget);
+  }
+
+
+  @override
+  void didChangeDependencies() {
+    debugPrint("didChangeDependencies=====");
+
+    super.didChangeDependencies();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: RaisedButton(
+        color: _getColors(),
+        child: widget.child,
+        onPressed: widget.onPressed,
+      ),
+    );
+  }
+}
+
+final _random = new Random();
+int next(int min, int max) => min + _random.nextInt(max - min);
+
+var colors = [
+  Colors.blue,
+  Colors.green,
+  Colors.orange,
+  Colors.purple,
+  Colors.amber,
+  Colors.lightBlue
+];
+
+var _buttonColors = <_FancyButtonState, Color>{};
